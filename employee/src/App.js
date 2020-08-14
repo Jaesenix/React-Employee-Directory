3310476f0da12f803553e2fce6345a73c084b4c0
Janella Senica <janella.senica@gmail.com>
2020-08-14 00:43:20 -0500

    add to App.js and src folders

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from "./components/Table.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Table} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Table />
+      </div>
+    )
+  }
 }
 
 export default App;
