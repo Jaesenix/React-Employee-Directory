b6f4ba410c8510c69acac15d562956822bcdefec
Janella Senica <janella.senica@gmail.com>
2020-08-16 23:15:35 -0500

    Updates

---
+++ precache-manifest.0edccb9061a1cb3bf5f9c228d22847eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f070fd8d8eb7e3377d2f0518d34e27",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "3170e763053bed69af19",
+    "url": "/React-Employee-Directory/static/css/main.61116a2b.chunk.css"
+  },
+  {
+    "revision": "1a87b73227cd1eed81b7",
+    "url": "/React-Employee-Directory/static/js/2.a3331a51.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Employee-Directory/static/js/2.a3331a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3170e763053bed69af19",
+    "url": "/React-Employee-Directory/static/js/main.0e6fc1fe.chunk.js"
+  },
+  {
+    "revision": "371398d0d801f407cabf",
+    "url": "/React-Employee-Directory/static/js/runtime-main.ca97982e.js"
+  }
+]);
