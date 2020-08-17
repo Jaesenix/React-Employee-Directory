9641699f305bee36c38b21d02995be39e0b02e93
Janella Senica <janella.senica@gmail.com>
2020-08-16 23:48:44 -0500

    rev styling

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -90,7 +90,7 @@ class EmployeeTable extends Component {
                                         age={employee.dob.age}
                                     />
 
-                                ))};
+                                ))}
 
                 </tbody>
                     </table>
