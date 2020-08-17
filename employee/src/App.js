915ce76c29a458ed89e0b47ed4c9d2c8ed589ae8
Janella Senica <janella.senica@gmail.com>
2020-08-16 21:29:27 -0500

    debugged errors

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Header from "./components/Header.js";
 import EmployeeTable from "./components/EmployeeTable.js";
-import SearchResults from './components/SearchResults.js';
 
 function App() {
   return (
